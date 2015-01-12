dd2989f884df5224ec1352e44840389436edf64a
Burt Beckwith <burt@burtbeckwith.com>
2015-01-12 04:54:35 -0500

    don't create a new instance of ChineseFriendlyUrlService - use dependency injection to get the initialized and configured Spring bean

--- test/integration/tw/georgemei/plugin/seo/friendly/urls/ChineseFriendlyUrlServiceIntegrationSpec.groovy
+++ test/integration/tw/georgemei/plugin/seo/friendly/urls/ChineseFriendlyUrlServiceIntegrationSpec.groovy
@@ -4,28 +4,24 @@ import grails.test.spock.IntegrationSpec
 
 class ChineseFriendlyUrlServiceIntegrationSpec extends IntegrationSpec {
 
+	def chineseFriendlyUrlService
+
 	def "String dynamic method is injected"() {
-		setup:
-		def chinesefriendlyUrlService = new ChineseFriendlyUrlService()
-				
 		expect:
 		string == "The Lord of the Rings"
-		string.asChineseFriendlyUrl() == chinesefriendlyUrlService.sanitizeWithDashes(string)
+		string.asChineseFriendlyUrl() == chineseFriendlyUrlService.sanitizeWithDashes(string)
 		string.asChineseFriendlyUrl() == "the-lord-of-the-rings"
-		
+
 		where:
 		string = "The Lord of the Rings"
 	}
 
 	def "String dynamic method is injected chinese"() {
-		setup:
-		def chinesefriendlyUrlService = new ChineseFriendlyUrlService()
-				
 		expect:
 		string == "灼眼的夏娜"
-		string.asChineseFriendlyUrl() == chinesefriendlyUrlService.chineseSanitizeWithDashes(string)
+		string.asChineseFriendlyUrl() == chineseFriendlyUrlService.chineseSanitizeWithDashes(string)
 		string.asChineseFriendlyUrl() == "burning-eyes-shakugan-no-shana"
-		
+
 		where:
 		string = "灼眼的夏娜"
 	}
